455193de5bdd7b29904ccb093624d075a0c6160b
AliSharifyofficial@gmail.com <Alisharifyofficial@gmail.com>
2026-06-14 14:22:39 +0330

    :construction: add base schemes for creating and retrieving tasks

--- app/schemas.py
+++ app/schemas.py
@@ -1,1 +1,25 @@
-# keep
+"""
+* task management
+* author: github.com/alisharify7
+* email: alisharifyofficial@gmail.com
+* license: see LICENSE for more details.
+* Copyright (c) 2026 - ali sharifi
+* https://github.com/alisharify7/Task-Sample-FioTrix
+"""
+
+import datetime
+
+from pydantic import BaseModel
+
+
+class CreateTaskSchem(BaseModel):
+    title: str
+    description: str | None  # Union[str, None]
+
+
+class DetailTaskSchem(BaseModel):
+    id: int
+    title: str
+    description: str | None  # Union[str, None]
+    is_complete: bool
+    created_at: datetime.datetime
