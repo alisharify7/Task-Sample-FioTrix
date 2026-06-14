9bac044c11580a78d71f670c12878e2c73ec6f30
AliSharifyofficial@gmail.com <Alisharifyofficial@gmail.com>
2026-06-14 14:41:38 +0330

    :sparkles: init base repo class for managing task related operations

---
+++ app/crud.py
@@ -0,0 +1,18 @@
+class TaskServiceRepository:
+    def __init__(self, db_session):
+        self.db_session = db_session
+
+    def add(self, task):
+        pass
+
+    def list(self):
+        pass
+
+    def destroy(self, task_id):
+        pass
+
+    def retrieve(self, task_id):
+        pass
+
+    def update(self, task):
+        pass
